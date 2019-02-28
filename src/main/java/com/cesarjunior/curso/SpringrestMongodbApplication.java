5a0e5ce500eb8ece961c43fef3350bec8e13cbff
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-02-28 20:48:55 -0300

    Criação e configuração do projeto

---
+++ src/main/java/com/cesarjunior/curso/SpringrestMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.cesarjunior.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestMongodbApplication.class, args);
+	}
+
+}
